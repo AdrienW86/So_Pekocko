c3a8c5950d215a9eda1fc68b8c156a35dd65187a
Adrien Weissenbacher <adrien_weiss@outlook.fr>
2020-12-21 17:37:51 +0100

    implémentation des différentes routes crud

--- backend/app.js
+++ backend/app.js
@@ -34,12 +34,30 @@ app.post('/api/sauces', (req, res, next) => {
       ...req.body
     });
     sauce.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
       .catch(error => res.status(400).json({ error }));
   });
 
+  app.put('/api/sauces/:id', (req, res, next) => {
+    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
 
-  app.use('/api/sauces', (req, res, next) => {
+  app.delete('/api/sauces/:id', (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  app.get('/api/sauces/:id', (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauce => res.status(200).json(sauce))
+      .catch(error => res.status(404).json({ error }));
+  });
+
+
+  app.get('/api/sauces', (req, res, next) => {
     Sauce.find()
       .then(sauces => res.status(200).json(sauces))
       .catch(error => res.status(400).json({ error }));
