4dec8a2894cca7db1c7b54496f43127d5a451866
unknown <adrien_weiss@outlook.fr>
2020-12-29 17:52:49 +0100

    ajout des dernieres commentaires

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,12 +10,13 @@ router.post('/signup',[
     check('email')
         .isEmail()
         .withMessage('Adresse email non valide')
-        .normalizeEmail(),
+        .normalizeEmail()
+        .matches(/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i),   // Vérification de l'email en interdisant les caractères pouvant servir à une injection
 
     check('password')
         .isLength({ min: 8, max: 20})
         .withMessage('votre mot de passe doit contenir entre 8 et 20 caractères')
-        ,
+        .matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$/), // Un Chiffre, une lettre majuscule et un symbole ($@%*+-_! uniquement)
 
 ], (req, res, next) => {
     const error = validationResult(req).formatWith(({msg}) => msg);
